da9cc6afb01f9073533595703288ecc02094e542
fre11896 <Austen.Freeman@Fluor.com>
2025-04-11 11:12:23 +0100

    Initial Chicken

---
+++ resources/js/main.js
@@ -0,0 +1,220 @@
+class player {
+    constructor(name, number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    goalsValue = 0;
+    assistsValue = 0;
+    savesValue = 0;
+    passesValue = 0;
+}
+
+let players = [];
+
+document.addEventListener("DOMContentLoaded", () => {
+    if (localStorage.length > 0) {
+        loadPlayers();
+    }
+});
+
+
+// #region LOAD
+
+function loadPlayers() {
+    for (let playerNumber = 1; playerNumber < localStorage.length + 1; playerNumber++) {
+
+        const player = getSettingObject(playerNumber);
+        if (player != null) {
+            players.push(player);
+            clonePlayerNodeAndSetup(player);
+        }
+    }
+}
+
+function setupGame() {
+
+    localStorage.clear();
+
+    players = [];
+
+    document.getElementById("playerslots").replaceChildren();
+}
+
+function createPlayers(numberOfPlayers) {
+    for (let playerNumber = 1; playerNumber < numberOfPlayers + 1; playerNumber++) {
+        let p = new player(playerNumber, (playerNumber == 1) ? true : false);
+
+        if (playerNumber == 1) {
+            // Game is being setup, human player is player 1 and has
+            // initiative and is set to be the current player
+            p.hasInitiative = true;
+            currentPlayer = p;
+        };
+
+        players.push(p);
+
+        clonePlayerNodeAndSetup(p);
+    }
+}
+
+function clonePlayerNodeAndSetup(player) {
+    const playertemplate = document.getElementById("playerTemplate").cloneNode(true);
+    playertemplate.id = "name_" + player.name;
+    playertemplate.classList.remove("d-none");
+    playertemplate.querySelector('#' + "playerName").innerHTML = player.name;
+
+    playertemplate.querySelector('#' + "playerGoals").innerHTML = player.goalsValue;
+    playertemplate.querySelector('#' + "playerGoals").id = player.name + "_Goals";
+
+    playertemplate.querySelector('#' + "playerAssists").innerHTML = player.assistsValue;
+    playertemplate.querySelector('#' + "playerAssists").id = player.name + "_Assists";
+
+    playertemplate.querySelector('#' + "playerSaves").innerHTML = player.savesValue;
+    playertemplate.querySelector('#' + "playerSaves").id = player.name + "_Saves";
+
+    playertemplate.querySelector('#' + "playerPasses").innerHTML = player.passesValue;
+    playertemplate.querySelector('#' + "playerPasses").id = player.name + "_Passes";
+
+
+    document.getElementById("playerslots").appendChild(playertemplate);
+}
+
+function addPlayer() {
+    let playerName = prompt("Please enter the player's name", "name");
+    if (playerName == null || playerName == "") {
+        //text = "User cancelled the prompt.";
+    } else {
+        const newPlayer = new player(playerName, localStorage.length + 1);
+        clonePlayerNodeAndSetup(newPlayer);
+        players.push(newPlayer);
+        SaveAllSettings();
+    }
+}
+
+function statClick(id) {
+    const div = document.getElementById(id);
+
+    let num = parseInt(div.innerHTML);
+    num += 1;
+
+    div.innerHTML = num;
+
+    const playerName = id.split("_");
+    const player = getPlayerByName(playerName[0]);
+
+    switch (playerName[1]) {
+        case "Goals":
+            player.goalsValue += 1;
+            break;
+
+        case "Assists":
+            player.assistsValue += 1;
+            break;
+
+        case "Saves":
+            player.savesValue += 1;
+            break;
+
+        case "Passes":
+            player.passesValue += 1;
+            break;
+
+        default:
+            break;
+    }
+
+    SaveAllSettings();
+}
+
+function getPlayerByName(playerName) {
+    let player;
+    players.forEach(p => {
+        if (p.name == playerName) {
+            player = p;
+        }
+    })
+
+    return player;
+}
+
+// #region SAVE
+
+const GetSettingsByValue = (val, includes = false) => {
+    let settings = new Array();
+    for (let [key, value] of Object.entries(localStorage)) {
+        if (includes && value.includes(val)) {
+            settings.push(key + "," + value);
+        } else if (value == val) {
+            settings.push(key + "," + value);
+        }
+    }
+    return settings;
+};
+
+const GetSettingsByKey = (val, includes = false) => {
+    let settings = new Array();
+    for (let [key, value] of Object.entries(localStorage)) {
+        if (includes && key.includes(val)) {
+            settings.push(key + "," + value);
+        } else if (key == val) {
+            settings.push(key + "," + value);
+        }
+    }
+    return settings;
+};
+
+const GetSettingValueByKey = (keytofind) => {
+    for (let [key, value] of Object.entries(localStorage)) {
+        if (key == keytofind) {
+            return value;
+        }
+    }
+
+    return "";
+};
+
+const SaveSetting = (key, value) => {
+    localStorage.setItem(key, value);
+};
+
+const SaveSettings = (settings) => {
+    for (let [key, value] of Object.entries(settings)) {
+        localStorage.setItem(key, value);
+    }
+
+};
+
+const SaveAllSettings = () => {
+    savePlayers();
+}
+
+const saveSettingObject = (settingName, settingObject) => {
+    localStorage.setItem(settingName, JSON.stringify(settingObject));
+};
+
+const getSettingObject = (settingName) => {
+    return JSON.parse(localStorage.getItem(settingName));
+};
+
+const RemoveSettingByKey = (k) => {
+    for (let [key, value] of Object.entries(localStorage)) {
+        if (key === k) {
+            localStorage.removeItem(key);
+        }
+    }
+};
+
+const RemoveSettingByValue = (val) => {
+    for (let [key, value] of Object.entries(localStorage)) {
+        if (value === val) {
+            localStorage.removeItem(key);
+        }
+    }
+};
+
+function savePlayers() {
+    players.forEach(player => {
+        saveSettingObject(player.number, player);
+    });
+}

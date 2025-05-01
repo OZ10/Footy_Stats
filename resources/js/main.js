044530a60194756739ce281f5d32b6010316c500
fre11896 <Austen.Freeman@Fluor.com>
2025-05-01 13:59:57 +0100

    Possible fix for Chrome Touch Support

--- resources/js/main.js
+++ resources/js/main.js
@@ -91,6 +91,7 @@ function clonePlayerNodeAndSetup(player) {
 let isLongClick = false;
 
 const startTimer = (event) => {
+    event.preventDefault();
     const button = document.getElementById(event.currentTarget.id);
     timer = setTimeout(() => {
         //alert('Long click detected!');
@@ -100,6 +101,7 @@ const startTimer = (event) => {
 };
 
 const clearTimer = (event) => {
+    event.preventDefault();
     const button = document.getElementById(event.currentTarget.id)
     clearTimeout(timer);
     if (!isLongClick) {
@@ -143,8 +145,9 @@ function cloneAndSetupButtons(playertemplate, player, buttonName) {
     //goalsButton.addEventListener('mouseleave', clearTimer);
 
     // Add touch events for mobile compatibility
-    button.addEventListener('touchstart', startTimer);
-    button.addEventListener('touchend', clearTimer);
+    button.addEventListener('touchstart', startTimer, { passive: false });
+    button.addEventListener('touchend', clearTimer, { passive: false });
+
     //goalsButton.addEventListener('touchcancel', clearTimer);
 }
 
